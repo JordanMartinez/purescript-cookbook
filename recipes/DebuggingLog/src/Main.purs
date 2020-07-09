e80f862a58260b55c03a0f5c934085f276bf5395
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-07-09 13:11:32 -0700

    Add more examples of `spy` in `traceM` examples

--- recipes/DebuggingLog/src/Main.purs
+++ recipes/DebuggingLog/src/Main.purs
@@ -100,11 +100,12 @@ usingTraceMInAff = do
 usingTraceMInAffRace :: Effect Unit
 usingTraceMInAffRace = do
   launchAff_ do
-    parSequence [ runXTimes 4 $ genValueTraceMNo 1
+    parSequence [ runXTimes 4 $ genValueTraceMNo $ spy "which fiber id" 1
                 , runXTimes 4 $ genValueTraceMYes 2
                 , runXTimes 4 $ genValueTraceMYes 3
                 , runXTimes 4 $ genValueTraceMYes 4
                 , runXTimes 4 $ genValueTraceMNo 5
+                , runXTimes 4 $ genValueSpy 5
                 ]
 
   where
@@ -120,4 +121,9 @@ usingTraceMInAffRace = do
       randomValue <- liftEffect $ randomInt 1 10
       delay $ Milliseconds $ toNumber randomValue
       traceM $ "Fiber: " <> show i <> " - random value: " <> show randomValue
+      pure unit
+
+    genValueSpy i = do
+      randomValue <- liftEffect $ randomInt 1 10
+      delay $ Milliseconds $ toNumber $ spy "(spy) random value" randomValue
       pure unit
