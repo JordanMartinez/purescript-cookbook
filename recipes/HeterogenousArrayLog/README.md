db646a2bd8e9e69a3de52f11c8da72e1da8cb0a7
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-07-13 06:56:50 -0700

    Add sum type version to compare with Variant version

--- recipes/HeterogenousArrayLog/README.md
+++ recipes/HeterogenousArrayLog/README.md
@@ -8,6 +8,17 @@ PureScript arrays must store values that have the same type. Attempting to store
 
 Prints the following:
 ```
+[Str(a String value),Integer(4),Bool(true),Bool(false),Num(82.4)]
+------------------
+["a String value","4","true","false","82.4"]
+------------------
+String value: a String value
+Int value: 4
+Boolean value: true
+Boolean value: false
+Number value: 82.4
+------------------
+------------------
 [(inj @"typeLevelString" "a String value"),(inj @"int" 4),(inj @"boolean" true),(inj @"boolean" false),(inj @"this type level string must match the label of the row" 82.4)]
 ------------------
 ["(inj @\"typeLevelString\" \"a String value\")","(inj @\"int\" 4)","(inj @\"boolean\" true)","(inj @\"boolean\" false)","(inj @\"this type level string must match the label of the row\" 82.4)"]
--- recipes/HeterogenousArrayLog/src/Main.purs
+++ recipes/HeterogenousArrayLog/src/Main.purs
@@ -13,6 +13,60 @@ separateWithDashes = log "------------------"
 
 main :: Effect Unit
 main = do
+  heterogenousArrayViaSumType
+
+  separateWithDashes
+  separateWithDashes
+
+  heterogenousArrayViaVariant
+
+data SumType
+  = Str String
+  | Integer Int
+  | Bool Boolean
+  | Num Number
+
+instance showSumType :: Show SumType where
+  show = case _ of
+    Str s -> "Str(" <> s <> ")"
+    Integer i -> "Integer(" <> show i <> ")"
+    Bool b -> "Bool(" <> show b <> ")"
+    Num n -> "Num(" <> show n <> ")"
+
+heterogenousArrayViaSumType :: Effect Unit
+heterogenousArrayViaSumType = do
+  let
+    heterogenousArray :: Array SumType
+    heterogenousArray =
+      [ Str "a String value"
+      , Integer 4
+      , Bool true
+      , Bool false
+      , Num 82.4
+      ]
+
+  -- works, but not quite what we want.
+  log $ show heterogenousArray
+  separateWithDashes
+
+  -- better, but does incur some boilerplate
+  log $ show $ heterogenousArray <#> \elem ->
+    case elem of
+      Str s -> s
+      Integer i -> show i
+      Bool b -> show b
+      Num n -> show n
+  separateWithDashes
+
+  foreachE heterogenousArray \elem -> do
+    log case elem of
+      Str s -> "String value: " <> s
+      Integer i -> "Int value: " <> show i
+      Bool b -> "Boolean value: " <> show b
+      Num n -> "Number value: " <> show n
+
+heterogenousArrayViaVariant :: Effect Unit
+heterogenousArrayViaVariant = do
   let
     -- Read: "This particular `SProxy` value has the type
     -- `SProxy "typeLevelString"`..."
