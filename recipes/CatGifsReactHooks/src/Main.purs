150c8916ae7ac5a11755deec231ab1eb660637ba
Peter Murphy <pete@simspace.com>
2020-07-18 12:27:14 -0400

    Fix broken CatGifsReactHooks recipe
    Resolves #183

--- recipes/CatGifsReactHooks/src/Main.purs
+++ recipes/CatGifsReactHooks/src/Main.purs
@@ -1,24 +1,22 @@
 module CatGifsReactHooks.Main where
 
 import Prelude
-import Affjax (printError)
 import Affjax as Affjax
 import Affjax.ResponseFormat as ResponseFormat
 import Data.Argonaut.Decode (decodeJson)
 import Data.Argonaut.Decode.Combinators ((.:))
-import Data.Array (singleton)
-import Data.Bifunctor (lmap)
-import Data.Either (Either(..))
-import Data.Maybe (Maybe(..))
+import Data.Either (hush)
+import Data.Maybe (Maybe(..), maybe)
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Exception (throw)
 import React.Basic.DOM (css, render)
 import React.Basic.DOM as R
 import React.Basic.Events (handler_)
-import React.Basic.Hooks (Component, component, useEffectOnce, (/\))
+import React.Basic.Hooks (Component, component, (/\))
 import React.Basic.Hooks as React
-import React.Basic.Hooks.Aff (AffReducer, useAffReducer)
+import React.Basic.Hooks.Aff (useAff)
+import React.Basic.Hooks.ResetToken (useResetToken)
 import Web.DOM.NonElementParentNode (getElementById)
 import Web.HTML (window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
@@ -28,20 +26,6 @@ data GifState
   = Failure
   | Loading
   | Success String
-
-data GifAction
-  = MorePlease
-  | GotGif (Either String String)
-
-reducer :: AffReducer GifState GifAction
-reducer state = case _ of
-  MorePlease ->
-    { state: Loading
-    , effects: [ (singleton <<< GotGif) <$> getRandomCatGif ]
-    }
-  GotGif result -> case result of
-    Right url -> { state: Success url, effects: [] }
-    Left _ -> { state: Failure, effects: [] }
 
 main :: Effect Unit
 main = do
@@ -55,33 +39,34 @@ main = do
 mkCatGifs :: Component {}
 mkCatGifs = do
   component "CatGifs" \_ -> React.do
-    (gifState /\ dispatch) <- useAffReducer Loading reducer
-    useEffectOnce do
-      dispatch MorePlease
-      pure mempty
+    (resetToken /\ reset) <- useResetToken
+    gifState <- toGifState <$> useAff resetToken getRandomCatGif
     let
-      onClick = handler_ (dispatch MorePlease)
+      onClick = handler_ reset
     pure case gifState of
+      Loading -> R.text "Loading..."
       Failure ->
         R.div_
           [ R.text "I could not load a random cat for some reason. "
           , R.button { onClick, children: [ R.text "Try Again!" ] }
           ]
-      Loading -> R.text "Loading..."
       Success url ->
         R.div_
           [ R.button { onClick, style: css { display: "block" }, children: [ R.text "More Please!" ] }
           , R.img { src: url }
           ]
 
-getRandomCatGif :: Aff (Either String String)
+-- | Collapse nested `Maybe`s to our `GifState` type
+toGifState :: Maybe (Maybe String) -> GifState
+toGifState = maybe Loading (maybe Failure Success)
+
+getRandomCatGif :: Aff (Maybe String)
 getRandomCatGif = do
   response <-
     Affjax.get
       ResponseFormat.json
       "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
   pure do
-    { body } <- lmap printError response
-    body' <- decodeJson body
-    data' <- body' .: "data"
-    data' .: "image_url"
+    -- Using `hush` to ignore the possible error messages
+    { body } <- hush response
+    hush (decodeJson body >>= (_ .: "data") >>= (_ .: "image_url"))
