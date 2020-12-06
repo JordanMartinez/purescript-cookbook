fbe850a6f10bd96de51ef4ad54ac13b846c8faeb
Ebmtranceboy <ebmtranceboy@gmail.com>
2020-12-06 14:30:18 +0100

    Add ReadPrintFileContentsConcur

---
+++ recipes/ReadPrintFileContentsConcur/README.md
@@ -0,0 +1,15 @@
+# ReadPrintFileContentsConcur
+
+A Concur recipe to open a local file and display its content in a web page.
+
+## Expected Behavior:
+
+When the input button is clicked, and a text file is chosen, the content of the file should be displayed on the page.
+
+### Browser
+
+The browser will display the text content as a <pre> element.
+
+## NPM Dependencies
+* [react](https://www.npmjs.com/package/react)
+* [react-dom](https://www.npmjs.com/package/react-dom)
---
+++ recipes/ReadPrintFileContentsConcur/src/Main.purs
@@ -0,0 +1,49 @@
+module ReadPrintFileContentsConcur.Main where
+
+import Prelude
+
+import Concur.Core (Widget)
+import Concur.React (HTML)
+import Concur.React.DOM  as D
+import Concur.React.Props  as P
+import Concur.React.Run (runWidgetInDom)
+import Data.Maybe (Maybe(..), maybe)
+import Effect (Effect)
+import Effect.Aff.Class (liftAff)
+import Effect.Class (liftEffect)
+import Web.File.File (toBlob)
+import Web.File.FileList (FileList, item)
+import Web.File.FileReader.Aff (readAsText)
+import Web.HTML.HTMLInputElement (files, fromEventTarget)
+import React.SyntheticEvent (target, SyntheticEvent_, NativeEventTarget)
+import Unsafe.Coerce (unsafeCoerce)
+import Web.Event.Internal.Types (EventTarget)
+
+fileList :: EventTarget -> Effect (Maybe FileList)
+fileList tar = case fromEventTarget tar of
+  Just elem -> files elem
+  _         -> pure Nothing
+
+getContent :: forall t
+  . Widget HTML (SyntheticEvent_ ( target :: NativeEventTarget
+                                 | t
+                                 )
+                ) -> Widget HTML String
+getContent ev = do
+  nativeTarget <- liftEffect =<< target <$> ev
+  mfs <- liftEffect $ fileList $ unsafeCoerce nativeTarget
+  liftAff $ maybe (pure "") 
+                       readAsText
+                       $ (Just <<< toBlob) =<< item 0 =<< mfs
+
+textFileToScreenWidget ∷ Widget HTML String
+textFileToScreenWidget = do
+  content <- getContent $ D.input [ P._type "file"
+                                  , P.onChange
+                                  ]
+  D.pre' [D.text content]
+  
+main :: Effect Unit
+main = do
+  runWidgetInDom "app" textFileToScreenWidget 
+
