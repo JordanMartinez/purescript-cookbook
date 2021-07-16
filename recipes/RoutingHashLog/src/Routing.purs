530eef09b894dea42830ca9f12289642ae352f10
Peter Murphy <pete@simspace.com>
2021-07-15 21:58:16 -0400

    Fix RoutingHashLog

--- recipes/RoutingHashLog/src/Routing.purs
+++ recipes/RoutingHashLog/src/Routing.purs
@@ -3,7 +3,7 @@ module RoutingHashLog.MyRouting where
 import Prelude
 import Data.Foldable (oneOf)
 import Data.Generic.Rep (class Generic)
-import Data.Generic.Rep.Show (genericShow)
+import Data.Show.Generic (genericShow)
 import Effect (Effect)
 import Effect.Class.Console (log)
 import Routing.Hash (matches)
