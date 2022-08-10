f6fb2cc5fb03dc82d2b73ffb71e60e7fd175b0f5
andys8 <andys8@users.noreply.github.com>
2022-08-10 19:39:55 +0200

    Update ComponentsInputReactHooks

--- recipes/ComponentsInputReactHooks/src/Main.purs
+++ recipes/ComponentsInputReactHooks/src/Main.purs
@@ -6,63 +6,57 @@ import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Exception (throw)
 import Effect.Unsafe (unsafePerformEffect)
-import React.Basic.DOM (render)
 import React.Basic.DOM as R
+import React.Basic.DOM.Client (createRoot, renderRoot)
 import React.Basic.Events (handler_)
-import React.Basic.Hooks
-  ( Component
-  , Render
-  , UseEffect
-  , UseRef
-  , component
-  , readRef
-  , useEffectAlways
-  , useRef
-  , useState
-  , writeRef
-  , (/\)
-  )
+import React.Basic.Hooks (Component, Render, UseEffect, UseRef, component, readRef, useEffectAlways, useRef, useState, writeRef, (/\))
 import React.Basic.Hooks as React
+import Web.DOM.NonElementParentNode (getElementById)
 import Web.HTML (window)
-import Web.HTML.HTMLDocument (body)
-import Web.HTML.HTMLElement (toElement)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
 import Web.HTML.Window (document)
 
 main :: Effect Unit
 main = do
-  body <- body =<< document =<< window
-  case body of
-    Nothing -> throw "Could not find body."
-    Just b -> do
-      container <- mkContainer
-      render (container unit) (toElement b)
+  doc <- document =<< window
+  root <- getElementById "root" $ toNonElementParentNode doc
+  case root of
+    Nothing -> throw "Could not find root."
+    Just container -> do
+      reactRoot <- createRoot container
+      app <- mkApp
+      renderRoot reactRoot (app {})
 
-mkContainer :: Component Unit
-mkContainer = do
+mkApp :: Component {}
+mkApp = do
   display <- mkDisplay
   component "Container" \_ -> React.do
     parentRenders <- useRenderCount
     state /\ setState <- useState 0
-    pure
-      $ R.div_
-          [ R.ul_
-              [ display state
-              , display (state * 2)
-              , display (state * 3)
-              , display (state * 10)
-              , display (state * state)
-              ]
-          , R.button
-              { onClick: handler_ (setState (_ + 1))
-              , children: [ R.text "+1" ]
-              }
-          , R.button
-              { onClick: handler_ (setState (_ - 1))
-              , children: [ R.text "-1" ]
-              }
-          , R.p_
-              [ R.text ("Parent has been rendered " <> show parentRenders <> " time(s)") ]
+    pure $ R.div_
+      [ R.ul_
+          [ display state
+          , display (state * 2)
+          , display (state * 3)
+          , display (state * 10)
+          , display (state * state)
           ]
+      , R.button
+          { onClick: handler_ (setState (_ + 1))
+          , children: [ R.text "+1" ]
+          }
+      , R.button
+          { onClick: handler_ (setState (_ - 1))
+          , children: [ R.text "-1" ]
+          }
+      , R.p_
+          [ R.text
+              ( "Parent has been rendered "
+                  <> show parentRenders
+                  <> " time(s)"
+              )
+          ]
+      ]
 
 mkDisplay :: Component Int
 mkDisplay =
@@ -80,6 +74,5 @@ useRenderCount = React.do
     renders <- readRef rendersRef
     writeRef rendersRef (renders + 1)
     pure mempty
-  let
-    renders = unsafePerformEffect (readRef rendersRef)
+  let renders = unsafePerformEffect (readRef rendersRef)
   pure renders
