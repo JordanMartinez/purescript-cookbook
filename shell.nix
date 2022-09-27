4d85b555b98ff85edd007f23417fc33cb18c05f3
Jacek Galowicz <jacek@galowicz.de>
2022-09-27 12:45:16 +0200

    Add nixpkgs and easy-purescript-nix as easily updateable niv sources

--- shell.nix
+++ shell.nix
@@ -1,22 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
-
 let
-  easy-ps = import (
-    pkgs.fetchFromGitHub {
-      owner = "justinwoo";
-      repo = "easy-purescript-nix";
-      # Latest `rev` and `sha256` can be obtained by using
-      # ```
-      # $ nix-prefetch-git https://github.com/justinwoo/easy-purescript-nix.git
-      # ```
-      rev = "bbef4245cd6810ea84e97a47c801947bfec9fadc";
-      sha256 = "00764zbwhbn61jwb5px2syzi2f9djyl8fmbd2p8wma985af54iwx";
-    }
-  ) {
-    inherit pkgs;
-  };
-
+  sources = import ./nix/sources.nix { };
+  pkgs = import sources.nixpkgs { };
+  easy-ps = import sources.easy-purescript-nix { inherit pkgs; };
 in
+
 pkgs.mkShell {
-  buildInputs = [ easy-ps.purs easy-ps.psc-package-simple easy-ps.spago easy-ps.spago2nix ];
+  buildInputs = with easy-ps; [
+    purs
+    psc-package-simple
+    spago
+    spago2nix
+  ] ++ (with pkgs; [
+    nodejs
+    niv
+  ]);
 }
